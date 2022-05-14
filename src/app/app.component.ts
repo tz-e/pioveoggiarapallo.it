2b8ea14d93fcad3fa67ccb44ce64653c24f95082
Daniele Atzeni <daniele.atzeni@flairbit.io>
2022-05-14 13:23:02 +0200

    First version of the website

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'pioveoggiarapallo.it';
+
+  private weatherData: any;
+  private lat: string = "44.35";
+  private lon: string = "9.233333";
+  private appId: string = "07f800126378a8cba38263e60cbfd1b1";
+
+  private rainingConditions: Array<string> = ['Thunderstorm', 'Drizzle', 'Rain', 'Snow'];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {   
+    this.http.get<any>(`https://api.openweathermap.org/data/2.5/weather?lat=${ this.lat }&lon=${ this.lon }&appid=${ this.appId }`)
+              .subscribe(data => {
+                  this.weatherData = data;
+    });
+}
+
+  public isRaining(){
+    return this.rainingConditions.includes(this.weatherData.weather[0].main);
+  }
+
+  public getClass(){
+    return this.isRaining()? 
+     'raining'
+     :
+     'not-raining';
+  }
+
+  public getLabel(){
+    return this.isRaining()? "Si": "No"
+  }
+}
